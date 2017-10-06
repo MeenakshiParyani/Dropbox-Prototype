a713d277e81b4324f4d9761354402c4fbace2683
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-06 13:17:11 -0700

    Css changes

--- Calculator/client/src/components/calculator.js
+++ Calculator/client/src/components/calculator.js
@@ -8,27 +8,27 @@ export default class Calculator extends Component {
         <div className="keypad">
           <div className="input-keys">
             <div className ="clear-key">
-              <button className="key-clear">AC</button>
+              <button className="key key-clear">AC</button>
             </div>
             <div className ="digit-keys">
-              <button className="key-0">0</button>
-              <button className="key-1">1</button>
-              <button className="key-2">2</button>
-              <button className="key-3">3</button>
-              <button className="key-4">4</button>
-              <button className="key-5">5</button>
-              <button className="key-6">6</button>
-              <button className="key-7">7</button>
-              <button className="key-8">8</button>
-              <button className="key-9">9</button>
+              <button className="key key-0">0</button>
+              <button className="key key-1">1</button>
+              <button className="key key-2">2</button>
+              <button className="key key-3">3</button>
+              <button className="key key-4">4</button>
+              <button className="key key-5">5</button>
+              <button className="key key-6">6</button>
+              <button className="key key-7">7</button>
+              <button className="key key-8">8</button>
+              <button className="key key-9">9</button>
             </div>
           </div>
           <div className="operation-keys">
-            <button className="key-divide">÷</button>
-            <button className="key-multiply">*</button>
-            <button className="key-subtract">-</button>
-            <button className="key-add">+</button>
-            <button className="key-equals">=</button>
+            <button className="key key-divide">÷</button>
+            <button className="key key-multiply">*</button>
+            <button className="key key-subtract">-</button>
+            <button className="key key-add">+</button>
+            <button className="key key-equals">=</button>
           </div>
         </div>
       </div>
