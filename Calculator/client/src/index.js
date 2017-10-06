cccfc43c154cb516bd7a75be469b68bd1ca48ff9
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-06 12:13:35 -0700

    Calculator Skeleton

--- Calculator/client/src/index.js
+++ Calculator/client/src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+import Calculator from './components/calculator';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<Calculator />, document.querySelector('.container'));
