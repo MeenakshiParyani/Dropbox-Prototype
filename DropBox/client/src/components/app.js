d628d5c62598a6f956303989053c4193df517c59
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-13 12:59:58 -0700

    Adding sinup page with formatting

--- DropBox/client/src/components/app.js
+++ DropBox/client/src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Signup from './signup';
 import Landing from './landing';
-
+import {Button} from 'react-bootstrap';
 export default class App extends Component {
   render() {
     return (
-      <div class = "container">
-        < Landing />
-        < Signup />
+      <div className = "container-fluid">
+        <div className="row">
+          <div className="col-md-8">< Landing /></div>
+          <div className="col-md-4">< Signup /></div>
+        </div>
       </div>
     );
   }
--- DropBox/client/src/components/signup.js
+++ DropBox/client/src/components/signup.js
@@ -3,7 +3,46 @@ import React, { Component } from 'react';
 export default class Signup extends Component {
   render() {
     return (
-      <div>Sign Up!!</div>
+      <div className="signup">
+      <form className="form-horizontal signup-form" role="form">
+              <h2>Sign Up</h2>
+              <div className="form-group">
+                  <div className="col-sm-9">
+                      <input type="text" id="firstName" placeholder="First Name" className="form-control" autofocus/>
+                  </div>
+              </div>
+              <div className="form-group">
+                  <div className="col-sm-9">
+                      <input type="text" id="lastName" placeholder="Last Name" className="form-control"/>
+                  </div>
+              </div>
+              <div className="form-group">
+                  <div className="col-sm-9">
+                      <input type="email" id="email" placeholder="Email" className="form-control"/>
+                  </div>
+              </div>
+              <div className="form-group">
+                  <div className="col-sm-9">
+                      <input type="password" id="password" placeholder="Password" className="form-control"/>
+                  </div>
+              </div>
+              <div className="form-group">
+                    <div className="col-sm-9">
+                        <div className="checkbox">
+                            <label>
+                                <input type="checkbox" id="terms" value="terms">Agree to Dropbox Terms</input>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-sm-9 col-sm-offset-3">
+                        <button type="submit" className="btn btn-primary btn-block btn-sign">Sign up</button>
+                    </div>
+                </div>
+              <br/><br/><br/><br/><br/><br/><br/><br/>
+          </form>
+      </div>
     );
   }
 }
