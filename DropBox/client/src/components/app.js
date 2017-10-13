d3e2fb7e28dc0b762f2c0d489056c130d05d3417
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-12 21:49:43 -0700

    Adding Signup page

---
+++ DropBox/client/src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Signup from './signup';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Signup />
+    );
+  }
+}
