4d49665681fd027d5812b958bef50d17c736082c
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-12 22:38:46 -0700

    Adding landling page

--- DropBox/client/src/components/app.js
+++ DropBox/client/src/components/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Signup from './signup';
+import Landing from './landing';
 
 export default class App extends Component {
   render() {
     return (
-      <Signup />
+      <div class = "container">
+        < Landing />
+        < Signup />
+      </div>
     );
   }
 }
