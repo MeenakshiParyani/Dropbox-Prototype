d68b5529ae362e617b1d5185dd8f2762a3b389b1
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-14 00:33:48 -0700

    clean up

---
+++ DropBox/client/src/components/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class HOME extends Component {
+
+  render() {
+    return (
+      <div className = "container-fluid">
+        MY HOME!!
+      </div>
+    );
+  }
+}
