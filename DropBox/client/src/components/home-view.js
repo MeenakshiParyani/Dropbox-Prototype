453d1a3ef1f1fe694490d35965767cc4124bc46a
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-15 05:01:42 -0700

    File Grid WIP

--- DropBox/client/src/components/home-view.js
+++ DropBox/client/src/components/home-view.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
-import {PageHeader, Grid, Row, Col} from 'react-bootstrap'
+import {PageHeader, Grid, Row, Col, Button} from 'react-bootstrap'
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import axios from 'axios';
 
@@ -19,16 +19,30 @@ class HomeView extends Component {
     }))
   }
 
+  dirFormatter(cell, row) {
+    if(cell){
+      return (
+        <button className="dir-btn"></button>
+      );
+    }else{
+      return (
+        <button>normal</button>
+      );
+    }
+
+  }
+
   render() {
 
     if(this.props.userFiles.length > 0){
       const columns = ["name", "isDir"];
       return (
-        <div className = "container-fluid">
+        <div className = "container-fluid ">
             <PageHeader className="header"><h3>Home</h3></PageHeader>
-            <BootstrapTable className="fileGrid" striped={true} data={this.props.userFiles} keyField='name'>
-              <TableHeaderColumn dataField='name'>Name</TableHeaderColumn>
-              <TableHeaderColumn dataField='isDir'>Dir</TableHeaderColumn>
+            <BootstrapTable headerStyle={{ display: 'none' }} tableStyle={{ margin: 0, borderRadius: 0, border: 0 }}
+             striped={false} sortable={true} data={this.props.userFiles} keyField='name'>
+              <TableHeaderColumn className="slim-width" dataField='isDir' dataFormat={ this.dirFormatter }>Active</TableHeaderColumn>
+              <TableHeaderColumn className="wide-width" dataField='name'>Name</TableHeaderColumn>
             </BootstrapTable>
         </div>
       );
