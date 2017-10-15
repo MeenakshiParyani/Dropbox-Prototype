e4a2c17f01543a73cb36cdcff6d84a8bb71d5b01
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-14 23:56:25 -0700

    Adding grid for displaying files

--- DropBox/client/src/components/home-view.js
+++ DropBox/client/src/components/home-view.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
-import {PageHeader} from 'react-bootstrap'
+import {PageHeader, Grid, Row, Col} from 'react-bootstrap'
 
 class HomeView extends Component {
 
@@ -10,6 +10,24 @@ class HomeView extends Component {
     return (
       <div className = "container-fluid">
           <PageHeader className="header"><h3>Home</h3></PageHeader>
+          <Grid>
+            <Row>
+              hi
+            </Row>
+            <Row key="count"
+                 style={{float: 'right', paddingRight: '50px'}} >
+              <div key="description">
+               hi1
+              </div>
+            </Row>
+            <Col style={{maxWidth: '225px'}}
+                 xs={12} sm={6} md={2} lg={2}>
+              hi2
+            </Col>
+            <Col xs={12} sm={6} md={10} lg={10}>
+              hi3
+            </Col>
+          </Grid>
       </div>
     );
   }
