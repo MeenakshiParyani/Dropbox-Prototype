fbb56f7e520370f514ef9b2e085300508a0a7c13
meenakshi.paryani@sjsu.edu <meenakshi.paryani@sjsu.edu>
2017-10-22 16:56:31 -0700

    cleanup

--- DropBox/app.js
+++ DropBox/app.js
@@ -20,26 +20,9 @@ var mongodb = require('./db/mongodb.js');
 
 // Express
 var app = express();
-// var whitelist = ['http://localhost:3000', 'http://localhost:8080']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   },
-//   methods : 'GET,HEAD,POST,PUT,DELETE,OPTIONS',
-//   credentials : true
-// }
-// app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-// // Start Mongo Db
-// mongoose.connect(mongodb.url, mongodb.options, true);
-
 
 // Save session
 app.use(session({
@@ -53,26 +36,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// // Add headers
-// app.use(function (req, res, next) {
-//     console.log('headers are');
-//     console.log(req.headers);
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', '*');
-//
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
-
-
 app.use(function(request, response, next) {
     response.header("Access-Control-Allow-Origin", request.headers.origin);
     response.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Authorization, X-Requested-With, userId");
@@ -80,7 +43,6 @@ app.use(function(request, response, next) {
     response.header("Access-Control-Allow-Credentials", "true");
     next();
 });
-
 
 //Routes
 app.use('/api/signup', signup);
